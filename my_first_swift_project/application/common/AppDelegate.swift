e1987acd1322a83b567be820d00a3eeccc7b3093
Tiago Henrique <tiagohenriqueodoia@gmail.com>
2019-12-07 18:58:37 -0300

    develop Add a inicial configuratoe for use xib
    -
    
    Signed-off-by: Tiago Henrique <tiagohenriqueodoia@gmail.com>

---
+++ my_first_swift_project/application/common/AppDelegate.swift
@@ -0,0 +1,37 @@
+//
+//  AppDelegate.swift
+//  my_first_swift_project
+//
+//  Created by Tiago Jácome Henrique do Carmo on 07/12/19.
+//  Copyright © 2019 Tiago Jácome Henrique do Carmo. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
---
+++ my_first_swift_projectTests/my_first_swift_projectTests.swift
@@ -0,0 +1,34 @@
+//
+//  my_first_swift_projectTests.swift
+//  my_first_swift_projectTests
+//
+//  Created by Tiago Jácome Henrique do Carmo on 07/12/19.
+//  Copyright © 2019 Tiago Jácome Henrique do Carmo. All rights reserved.
+//
+
+import XCTest
+@testable import my_first_swift_project
+
+class my_first_swift_projectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
